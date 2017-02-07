5e2701beaf7fe9c4c6ab70f03a026a4986bef3c8
William Godard <grenier.godard@gmail.com>
2017-02-07 23:11:20 +0100

    rails new

---
+++ app/models/product.rb
@@ -0,0 +1,3 @@
+class Product < ApplicationRecord
+  belongs_to :line_item
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207212134) do
+ActiveRecord::Schema.define(version: 20170207215628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "photo"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
