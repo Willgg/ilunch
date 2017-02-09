4ebf44b234f76b42c2114bacd6aa1e1361efb6f4
William Godard <grenier.godard@gmail.com>
2017-02-09 13:57:38 +0100

    Add figaro and create admin views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :products, only: [:index]
+
+  namespace :admin do
+    get '', to: 'dashboard#index', as: '/'
+  end
 end
