d3dee33fd3331ba0747644b70f8542d1ed1e08b9
William Godard <grenier.godard@gmail.com>
2017-02-10 19:24:21 +0100

    Add product actions in admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,39 @@
 class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :destroy]
+
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = 'Le produit a bien été ajouté !'
+      redirect_to admin_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_path
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :photo)
   end
 end
