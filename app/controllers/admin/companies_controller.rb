fb09920ae194e450e697e974ad89b28b9d839304
William Godard <grenier.godard@gmail.com>
2017-02-12 22:49:40 +0100

    Add companies to admin

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -5,12 +5,29 @@ class Admin::CompaniesController < ApplicationController
     @companies = Company.paginate(:page => params[:page], :per_page => 30)
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to admin_companies_path
+    else
+      render :new
+    end
+  end
+
   def destroy
     @company.destroy
     redirect_to admin_companies_path
   end
 
   private
+
+  def company_params
+    params.require(:company).permit(:name, :street, :post_code, :delivery_time)
+  end
 
   def set_company
     @company = Company.find(params[:id])
