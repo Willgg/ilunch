076f9cceeca5f715b9405b6a5c0c452a408d76f4
William Godard <grenier.godard@gmail.com>
2017-02-13 19:05:18 +0100

    Add victoria account in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,6 +54,10 @@ user_1 = User.create( first_name: 'William', last_name: 'Grenier Godard',
                       email: 'grenier.godard@gmail.com', password: 'billyboy',
                       optin: true )
 
+user_2 = User.create( first_name: 'Victoria', last_name: 'Benhaim',
+                      email: 'victoria.startup@gmail.com', password: 'ilunch',
+                      optin: true )
+
 company_1 = Company.create( name: 'Le Village', street: '55 rue de la boétie',
                             post_code: '75008', delivery_time: Company::TIMESLOT.sample )
 
