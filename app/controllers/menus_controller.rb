b8ef9e6703a0b188e206f0557cbec59aefc8b98b
William Godard <grenier.godard@gmail.com>
2017-03-28 12:30:47 +0200

    Update homepage

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,6 +6,7 @@ class MenusController < ApplicationController
     @menus = policy_scope(Menu)
     date = params[:date].nil? ? Date.today : Date.parse(params[:date])
     @products = Product.category('main').available_for(date)
+    @products_of_week = Product.category('main').of_the_week.sample(5)
     @line_item = LineItem.new
   end
 end
