a5fddb23f68c573b84b200175555ee6c417e6f3a
William Godard <grenier.godard@gmail.com>
2017-04-03 15:47:13 +0200

    Sort by params in admin

--- app/controllers/admin/line_items_controller.rb
+++ app/controllers/admin/line_items_controller.rb
@@ -1,6 +1,8 @@
 class Admin::LineItemsController < ApplicationController
 
   def index
-    @line_items = policy_scope(LineItem).paginate(:page => params[:page], :per_page => 30)
+    @line_items = policy_scope(LineItem)
+    @line_items = @line_items.where(order_id: params[:order]) if params[:order]
+    @line_items = @line_items.paginate(:page => params[:page], :per_page => 30)
   end
 end
