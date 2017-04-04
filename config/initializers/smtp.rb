c05ac8563d7df9505e16f433a1b96d8da0d7db18
William Godard <grenier.godard@gmail.com>
2017-04-04 10:41:41 +0200

    Add new smtp config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   user_name: ENV['POSTMARK_API_KEY'],
   password: ENV['POSTMARK_API_TOKEN'],
   address: ENV['POSTMARK_SMTP_SERVER'],
-  port: '2525',
-  domain: 'www.ilunch.fr',
+  port: '25',
+  domain: 'ilunch-live.herokuapp.com',
   authentication: 'plain',
   enable_starttls_auto: true
 }
