2fa4e5d3f1f255c8cb3b5f72dcda6146faedb048
William Godard <grenier.godard@gmail.com>
2017-04-04 10:03:39 +0200

    Change smtp port in initializer

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   user_name: ENV['POSTMARK_API_KEY'],
   password: ENV['POSTMARK_API_TOKEN'],
   address: ENV['POSTMARK_SMTP_SERVER'],
-  port: 587,
+  port: 25,
   domain: 'ilunch.fr',
   authentication: 'plain',
   enable_starttls_auto: true
