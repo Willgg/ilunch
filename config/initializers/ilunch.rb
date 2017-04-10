913b468b05b0d21d8587908ef0143742a4ccdcb2
William Godard <grenier.godard@gmail.com>
2017-04-10 11:54:28 +0200

    Update Ilunch module to be closed for next days

--- config/initializers/ilunch.rb
+++ config/initializers/ilunch.rb
@@ -12,15 +12,16 @@ module Ilunch
     return week.first(number)
   end
 
-  def self.open?
-    if ENV['RAILS_ENV'] == 'production'
+  def self.open?(date=nil)
+    # return true if ENV['RAILS_ENV'] != 'production'
+    if date.nil?
       DateTime.current <= DateTime.current.beginning_of_day + 12.hours
     else
-      true
+      date == Date.current
     end
   end
 
-  def self.closed?
-    !open?
+  def self.closed?(date=nil)
+    !open?(date)
   end
 end
