cdd7fea2f0d61e8acd95e4cebd71d4c385113e1c
William Godard <grenier.godard@gmail.com>
2017-04-12 00:17:20 +0200

    Fix Date.today for helper next_days

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -1,7 +1,7 @@
 module MenusHelper
   def next_days(number)
     week = []
-    date = Date.today
+    date = Date.current
     until week.count == number do
       week << date if ( date.wday != 6 && date.wday != 0 )
       date += 1.day
