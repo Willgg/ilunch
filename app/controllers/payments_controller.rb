599c77a9fbb44632367b4e63228b58f32eaee588
William Godard <grenier.godard@gmail.com>
2017-04-13 18:28:43 +0200

    Cancel payment if product no longer in stock

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,44 +10,49 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    if authorize(@order, session[:order_id], :create?)
-      begin
-
-        if current_user.stripe_id.nil?
-          customer = Stripe::Customer.create(
-            source: params[:stripeToken],
-            email:  params[:stripeEmail]
-          )
-          current_user.update(stripe_id: customer.id) if customer.id
-        end
-
-        charge = Stripe::Charge.create(
-          customer:     current_user.stripe_id, # You should store this customer id and re-use it.
-          amount:       @order.total_price_cents, # or amount_pennies
-          description:  "Payment for order#{@order.id}",
-          currency:     @order.total_price.currency
-        )
-
-        case charge.status
-        when 'succeeded'
-          @order.update(payment: charge.to_json, status: 1)
-          @order.substract_products_stocks
-          @order.send_confirmation_email
-          session.delete(:order_id)
-        when 'pending'
-          @order.update(payment: charge.to_json, status: 0)
-        when 'failed'
-          @order.update(payment: charge.to_json, status: 2)
-        end
-
-        redirect_to order_path(@order)
-
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to new_order_payment_path(@order)
-      end
+    if @order.products_out_of_stock.present?
+      flash[:alert] = "Le produit n'est plus en stock"
+      redirect_to new_order_payment_path(@order)
     else
-      redirect_to products_path
+      if authorize(@order, session[:order_id], :create?)
+        begin
+
+          if current_user.stripe_id.nil?
+            customer = Stripe::Customer.create(
+              source: params[:stripeToken],
+              email:  params[:stripeEmail]
+            )
+            current_user.update(stripe_id: customer.id) if customer.id
+          end
+
+          charge = Stripe::Charge.create(
+            customer:     current_user.stripe_id, # You should store this customer id and re-use it.
+            amount:       @order.total_price_cents, # or amount_pennies
+            description:  "Payment for order#{@order.id}",
+            currency:     @order.total_price.currency
+          )
+
+          case charge.status
+          when 'succeeded'
+            @order.update(payment: charge.to_json, status: 1)
+            @order.substract_products_stocks
+            @order.send_confirmation_email
+            session.delete(:order_id)
+          when 'pending'
+            @order.update(payment: charge.to_json, status: 0)
+          when 'failed'
+            @order.update(payment: charge.to_json, status: 2)
+          end
+
+          redirect_to order_path(@order)
+
+        rescue Stripe::CardError => e
+          flash[:error] = e.message
+          redirect_to new_order_payment_path(@order)
+        end
+      else
+        redirect_to products_path
+      end
     end
   end
 
