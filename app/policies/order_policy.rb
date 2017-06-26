6a9fa50d133d93fc50a9f94bb07e55a566ea2e24
William Godard <grenier.godard@gmail.com>
2017-06-26 19:18:31 +0200

    Fix condition policy order

--- app/policies/order_policy.rb
+++ app/policies/order_policy.rb
@@ -14,7 +14,7 @@ class OrderPolicy < ApplicationPolicy
   def show?
     status = ['payed', 'ready', 'delivered'].include? @record.status
     ownership =  @record.user ? ( @record.user == @user ) : ( @record.id == @session )
-    (status && ownership) || @user.admin?
+    (@user.admin?) || (status && ownership)
   end
 
   def create?
